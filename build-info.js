6e06bfa1e2f8222226a95cc101a09b2d374974f3
AgenticLab-SH <77868633+AgenticLab-SH@users.noreply.github.com>
2026-04-10 02:22:47 +0900

    Refine modal help flows and popup guidance

--- build-info.js
+++ build-info.js
@@ -1,5 +1,5 @@
 window.SKCTBuildInfo = Object.freeze({
-    updatedAt: '2026-04-10 01:49:00 +09:00',
-    version: 'v2026.04.10.0149',
-    assetVersion: '202604100149'
+    updatedAt: '2026-04-10 02:19:08 +09:00',
+    version: 'v2026.04.10.0219',
+    assetVersion: '202604100219'
 });
