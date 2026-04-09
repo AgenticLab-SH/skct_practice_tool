9b883902b5efa07b954a9d0e5abdea4add3592da
AgenticLab-SH <77868633+AgenticLab-SH@users.noreply.github.com>
2026-04-09 15:21:36 +0900

    fix: remove inline advanced status panel

--- build-info.js
+++ build-info.js
@@ -1,5 +1,5 @@
 window.SKCTBuildInfo = Object.freeze({
-    updatedAt: '2026-04-09 14:54:48 +09:00',
-    version: 'v2026.04.09.1454',
-    assetVersion: '202604091454'
+    updatedAt: '2026-04-09 15:19:50 +09:00',
+    version: 'v2026.04.09.1519',
+    assetVersion: '202604091519'
 });
