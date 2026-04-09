821ec4d3694b7f6364a2c7cbcbbd58bea6bdee96
AgenticLab-SH <77868633+AgenticLab-SH@users.noreply.github.com>
2026-04-10 01:51:22 +0900

    deploy: refresh public clean bundle for popup guide cleanup

--- build-info.js
+++ build-info.js
@@ -1,5 +1,5 @@
 window.SKCTBuildInfo = Object.freeze({
-    updatedAt: '2026-04-10 10:45:00 +09:00',
-    version: 'v2026.04.10.1045',
-    assetVersion: '202604101045'
+    updatedAt: '2026-04-10 01:49:00 +09:00',
+    version: 'v2026.04.10.0149',
+    assetVersion: '202604100149'
 });
