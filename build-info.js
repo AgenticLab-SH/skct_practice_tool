e947c77cc91a5cb901723054f11b114fe6fb8bdc
AgenticLab-SH <77868633+AgenticLab-SH@users.noreply.github.com>
2026-04-09 17:28:44 +0900

    fix: restore notepad text cursor

--- build-info.js
+++ build-info.js
@@ -1,5 +1,5 @@
 window.SKCTBuildInfo = Object.freeze({
-    updatedAt: '2026-04-09 16:40:00 +09:00',
-    version: 'v2026.04.09.1640',
-    assetVersion: '202604091640'
+    updatedAt: '2026-04-09 17:45:00 +09:00',
+    version: 'v2026.04.09.1745',
+    assetVersion: '202604091745'
 });
