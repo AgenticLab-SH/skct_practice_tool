a982d95d9f60c28f4229d385b7b2b75e6e6e390e
AgenticLab-SH <77868633+AgenticLab-SH@users.noreply.github.com>
2026-04-10 13:15:35 +0900

    deploy: publish unified help flow cleanup

--- build-info.js
+++ build-info.js
@@ -1,5 +1,5 @@
 window.SKCTBuildInfo = Object.freeze({
-    updatedAt: '2026-04-10 11:24:28 +09:00',
-    version: 'v2026.04.10.1124',
-    assetVersion: '202604101124'
+    updatedAt: '2026-04-10 13:13:18 +09:00',
+    version: 'v2026.04.10.1313',
+    assetVersion: '202604101313'
 });
